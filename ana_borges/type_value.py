37a8c8b60a883e314627b7175b121e92f610503b
Ana Borges <ana.borges@ab-inbev.com>
2021-05-20 08:38:20 -0300

    [BEESQM-5204]Create Method to print types

---
+++ ana_borges/type_value.py
@@ -0,0 +1,15 @@
+#Challenge 2 - Ana & Mari
+
+def print_types(first_input, second_input):
+    try:
+        first_type = type(eval(first_input))
+    except:
+        first_type = type('String')
+    try:
+        second_type = type(eval(second_input))
+    except:
+        second_type = type('String')
+    print("The first value type is {} and the second one is {}".format(first_type, second_type))
+first_input = input("Input the first value:")
+second_input = input("Input the second value:")
+print_types(first_input, second_input)
