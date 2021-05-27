cfa41127fa7b0062993e6040fbbffe6861d8ecff
Rafaela Piellusch <rafaelapie@hotmail.com>
2021-05-27 09:41:26 -0300

    [BEESQM-5288] Challenge 4 (#35)
    Co-authored-by: Rafaela Piellusch <rafaela.piellusch@ab-inbev.com>

--- rafaela_piellusch/calc.py
+++ rafaela_piellusch/calc.py
@@ -1,63 +1,118 @@
+import itertools
 import sys
 
-def linha():
-    print('-'*70)
+
+def line():
+    print('-' * 80)
 
 
-def calculadora():
-    linha()
-    while True:
-        try:
-            n1 = float(input('Primeiro valor: '))
-            linha()
-            n2 = float(input('Segundo valor: '))
-            linha()
-        except(ValueError, TypeError):
-            print('ERRO: Por favor, digite um número válido!')
-            continue
+def add(x, y):
+    return x + y
 
-        opção = 0
-        while opção != 6:
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+def calculate():
+    for c in itertools.count(start=0):
+        history = []
+        while True:
             try:
-                print(''' 
-                    [ 1 ] Somar
-                    [ 2 ] Subtrair
-                    [ 3 ] Multiplicar
-                    [ 4 ] Dividir
-                    [ 5 ] Inserir os valores novamente
-                    [ 6 ] Sair
-                    ''')
-                linha()
-                opção = int(input('>>>>> Digite sua opção: '))
-                linha()
+                line()
+                num1 = float(input("Enter first number: "))
+                line()
+                num2 = float(input("Enter second number: "))
             except(ValueError, TypeError):
-                print('ERRO: Por favor, digite um número válido!')
+                print('INVALID INPUT')
+                line()
                 continue
+            choice = 0
+            while choice != 6:
+                try:
+                    line()
+                    print('''CHOOSE YOUR OPTIONS:
+                                    [ 1 ] ADD
+                                    [ 2 ] SUBTRACT
+                                    [ 3 ] MULTIPLY
+                                    [ 4 ] DIVIDE
+                                    [ 5 ] INPUT THE NUMBERS AGAIN
+                                    [ 6 ] HISTORY
+                                    [ 7 ] EXIT''')
+                    line()
+                    choice = input("Enter choice: ")
 
-            if opção == 1:
-                soma = n1 + n2
-                print('A soma entre {} e {} é {}'.format(n1, n2, soma))
-            elif opção == 2:
-                produto = n1 - n2
-                print('A subtração de {} e {} é {}'.format(n1, n2, produto))
-            elif opção == 3:
-                vezes = n1 * n2
-                print('A multiplicação de {} e {} é {}'.format(n1, n2, vezes))
-            elif opção == 4:
-                if n1 and n2 > 0:
-                    divisao = n1 / n2
-                    print('A divisao de {} e {} é {}'.format(n1, n2, divisao))
-                else:
-                    print('Não podemos operar divisão 0')
-            elif opção == 5:
-                print('Informe os números novamente: ')
-                n1 = int(input('Primeiro valor: '))
-                n2 = int(input('Segundo valor: '))
-            elif opção == 6:
-                print('Finalizando....')
-                sys.exit()
-            else:
-                print('Opção inválida, tente novamente')
+                    if choice in ('1', '2', '3', '4', '5', '6', '7'):
+
+                        if choice == '1':
+                            line()
+                            print(num1, "+", num2, "=", add(num1, num2))
+                            c += 1
+                            history.append({'num1': num1, 'num2': num2, 'choice': 'add', 'position': c})
+
+                        elif choice == '2':
+                            line()
+                            print(num1, "-", num2, "=", subtract(num1, num2))
+                            c += 1
+                            history.append({'num1': num1, 'num2': num2, 'choice': 'subtract', 'position': c})
+
+                        elif choice == '3':
+                            line()
+                            print(num1, "*", num2, "=", multiply(num1, num2))
+                            c += 1
+                            history.append({'num1': num1, 'num2': num2, 'choice': 'multiply', 'position': c})
+
+                        elif choice == '4':
+                            line()
+                            print(num1, "/", num2, "=", divide(num1, num2))
+                            c += 1
+                            history.append({'num1': num1, 'num2': num2, 'choice': 'divide', 'position': c})
+
+                        elif choice == '5':
+                            line()
+                            num1 = float(input("Enter first number: "))
+                            num2 = float(input("Enter second number: "))
+
+                        elif choice == '6':
+                            line()
+                            print(''' HISTORY:
+                                    Press 1 if you wanna see all history
+                                    Press 2 if you wanna see just a position
+                                    ''')
+                            line()
+                            choice1 = int(input("Enter choice: "))
+
+                            if choice1 == 1:
+                                line()
+                                print(history)
+                            elif choice1 == 2:
+                                line()
+                                position_hist = int(input('What position do you want? '))
+                                if len(history) >= position_hist > 0:
+                                    position_hist -= 1
+                                    line()
+                                    print(history[position_hist])
+                            else:
+                                line()
+                                print('INVALID INPUT')
+
+                        elif choice == '7':
+                            line()
+                            print('Thank you....')
+                            line()
+                            sys.exit()
+
+                except(ValueError, TypeError):
+                    line()
+                    print('INVALID INPUT')
 
 
-calculadora()
+calculate()
