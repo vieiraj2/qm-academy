db70f913513de6eac416f4fd4e7907a67f61673b
Raul Cunha <raul.cunha@ab-inbev.com>
2021-05-31 11:30:27 -0300

    [BEESQM-5405] [feat] Create a new Improvement for the calc.py project, adapting the code to include the following improvements: 1) Allows the user to display all operations executed previously (history of operations); 2) Allows the user to verify a specific operation according to it's position.

--- raul_cunha/calc.py
+++ raul_cunha/calc.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # Program to create a Calculator for integer numbers - Addition, Subtraction, Multiplication and Division
-continua = True
+# With IMPROVEMENTS - History of operations
+continua = True     # variavel de controle do loop principal WHILE
+Historic = []      # Lista p/ armazenamento de informações de operações para Histórico
 while continua:
     oper_valids = False
     oper1 = oper2 = 1
@@ -10,47 +12,117 @@ while continua:
     print('2 - Subtraction')
     print('3 - Multiplication')
     print('4 - Division')
-    print('5 - Quit')
+    print('5 - Historic of Operations')
+    print('6 - Quit')
     operacao = input('Type the option of operation, and after press <ENTER>: ')
     print('\n')
-    if  operacao == '5':
+    if  operacao == '6':
         continua = False
         continue
+    elif  operacao == '5':
+        if len(Historic) == 0:
+            print('Historic of Operations is empty!')
+            print('\n')
+            continue
+        print('** Historic of Operations **')
+        #   print('\n')
+        print('1 - All Operations')
+        print('2 - Choose a specific operation')
+        print('3 - Return')
+        option_hist = input('Type the option for Historic, and after press <ENTER>: ')
+
+        if option_hist == '1':
+            print('EXIBIR TODO HISTORICO')
+            for j in range(len(Historic)):
+                #print('Operation ', j+1, ': ', Historic[j])
+                print(Historic[j])
+                print('\n')
+        elif option_hist == '2':
+            try:
+                option_oper = int(input('Type which operation would you like to search: '))
+            except:
+                print('Invalid input(s)!')
+                print('\n')
+                continue
+            if option_oper != 0:
+                print(Historic[option_oper - 1])
+                print('\n')
+            else:
+                print('Invalid input(s)!')
+        else:
+            print('\n')
+            continue
+
     elif  operacao == '1':
         print('* Addition *')
         try:
             oper1 = int(input('Input the first operator, and after press <ENTER>: '))
-            oper2 = int(input('Input the first operator, and after press <ENTER>: '))
-            print('Result of operation: ', oper1 + oper2)
+            oper2 = int(input('Input the second operator, and after press <ENTER>: '))
         except:
             print('Invalid input(s)!')
+            print('\n')
+            continue
+        print('Result of operation: ', oper1 + oper2)
+        # GUARDA NO HISTORICO
+        resultado = oper1 + oper2
+        content = (oper1, " + ", oper2, ' = ', resultado)
+        Historic.append(content)
+        print('\n')
+
     elif operacao == '2':
         print('* Subtraction *')
         try:
             oper1 = int(input('Input the first operator, and after press <ENTER>: '))
-            oper2 = int(input('Input the first operator, and after press <ENTER>: '))
-            print('Result of operation: ', oper1 - oper2)
+            oper2 = int(input('Input the second operator, and after press <ENTER>: '))
         except:
             print('Invalid input(s)!')
+            print('\n')
+            continue
+        print('Result of operation: ', oper1 - oper2)
+        # GUARDA NO HISTORICO
+        resultado = oper1 - oper2
+        content = (oper1, " - ", oper2, ' = ', resultado)
+        Historic.append(content)
+        print('\n')
+
     elif operacao == '3':
         print('* Multiplication *')
         try:
             oper1 = int(input('Input the first operator, and after press <ENTER>: '))
-            oper2 = int(input('Input the first operator, and after press <ENTER>: '))
-            print('Result of operation: ', oper1 * oper2)
+            oper2 = int(input('Input the second operator, and after press <ENTER>: '))
         except:
             print('Invalid input(s)!')
+            print('\n')
+            continue
+        print('Result of operation: ', oper1 * oper2)
+        # GUARDA NO HISTORICO
+        resultado = oper1 * oper2
+        content = (oper1, " * ", oper2, ' = ', resultado)
+        Historic.append(content)
+        print('\n')
+
     elif operacao == '4':
         print('* Division *')
         try:
             oper1 = int(input('Input the first operator, and after press <ENTER>: '))
-            oper2 = int(input('Input the first operator, and after press <ENTER>: '))
-            if oper2 != 0:
-                print('Result of operation: ', oper1 / oper2)
-            else:
-                print('It does not exist a division by zero!')
+            oper2 = int(input('Input the second operator, and after press <ENTER>: '))
+
         except:
             print('Invalid input(s)!')
+            print('\n')
+            continue
+        if oper2 != 0:
+            print('Result of operation: ', oper1 / oper2)
+        else:
+            print('It does not exist a division by zero!')
+            print('\n')
+            continue
+        # GUARDA NO HISTORICO
+        resultado = oper1 / oper2
+        content = (oper1, " / ", oper2, ' = ', resultado)
+        Historic.append(content)
+        print('\n')
+
     else:
         print('Invalid option!')
 
