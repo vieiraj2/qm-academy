4f5331d4370f237e4cfcb8417ed58cfaf7dd547b
William Ribeiro Moreno <william.ribeiro@ab-inbev.com>
2021-05-31 13:06:30 -0300

    [BEESQM-5423][feat] Create calculator, calculator_concat_sum classes + exception for division by zero

--- william_moreno/calculator_concat_sum.py
+++ william_moreno/calculator_concat_sum.py
@@ -5,5 +5,5 @@ class CalculatorConcatSum(Calculator):
 
     def sum(self):
         print(f"O resultado da soma concatenada é {str(self._a) + str(self._b)}\n")
-        op_result = [self._a, "+", self._b, "=", (str(self._a) + str(self._b))]
-        return op_result
+        opResult = [self._a, "+", self._b, "=", (str(self._a) + str(self._b))]
+        return opResult
