05c4acb8c91dcbf294028e4d88fc19b4edc6be75
Ana Cristina <77416348+anacriscborges@users.noreply.github.com>
2021-06-01 15:39:11 -0300

    Feature/BEESQM-5399 (#41)
    * [BEESQM-5225]Create calculator (#30)
    
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    
    * [BEESQM-5224]Create calculator - Challenge 3 (#29)
    
    Co-authored-by: Ana Borges <ana.borges@ab-inbev.com>
    
    * BEESQM-5399 - Challenge 5
    
    * BEESQM-5399 Challenge 5
    
    * BEESQM-5399
    
    * BEESQM-5399
    
    Co-authored-by: Mariane-Yonue <mariane.yonue@gmail.com>
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    Co-authored-by: Ana Borges <ana.borges@ab-inbev.com>

--- ana_borges/calc.py
+++ ana_borges/calc.py
@@ -1,68 +1,63 @@
-#Challenge 4 - Ana & Mari
+#Challenge 5 - Ana & Mari
 
-
+from calculator_concat_sum import CalculatorConcatSum
 def operation():
-   result = None
-   is_denominator_zero = False
-   not_a_number = False
-   try:
-      n1 = float(input('First number:'))
-      n2 = float(input('Second number:'))
-      operator = input('''\nChoose the type of operation you want:
-         '+' Sum
-         '-' Subtraction
-         '*' Multiplication
-         '/' Division\n''')
-      if operator == '+':
-         result = n1 + n2
-      elif operator == '-':
-         result = n1 - n2
-      elif operator == '*':
-         result = n1 * n2
-      elif operator == '/' and n2 == 0:
-         is_denominator_zero = True
-      elif operator == '/':
-         result = n1 / n2
-   except:
-      not_a_number = True
-   if is_denominator_zero:
-      print ('Can\'t divide by zero! Try again')
-   elif not_a_number:
-      print ('It\'s not a number!')
-   elif result == None:
-      print ('Invalid option!')
-   else:
-      operation_string = f'{n1} {operator} {n2} = {result}'
-      print(f'You chose \'{operator}\'')
-      print(operation_string)
-      history.append(operation_string)
-      
-      
+    result = None
+    is_denominator_zero = False
+    not_a_number = False
+    try:
+        n1 = float(input('First number:'))
+        n2 = float(input('Second number:'))
+        operator = input('''\nChoose the type of operation you want:
+            '+' Sum
+            '-' Subtraction
+            '*' Multiplication
+            '/' Division\n''')
+        calculator = CalculatorConcatSum(n1, n2)
+        if operator == '+':
+            result = calculator.sum()
+        elif operator == '-':
+            result = calculator.sub()
+        elif operator == '*':
+            result = calculator.mult()
+        elif operator == '/' and n2 == 0:
+            is_denominator_zero = True
+        elif operator == '/':
+            result = calculator.div()
+    except:
+        not_a_number = True
+    if is_denominator_zero:
+        print ('Can\'t divide by zero! Try again')
+    elif not_a_number:
+        print ('It\'s not a number!')
+    elif result == None:
+        print ('Invalid option!')
+    else:
+        operation_string = f'{n1} {operator} {n2} = {result}'
+        history.append(operation_string)
 is_calculator_on = True
 history = []
-
-
 while is_calculator_on:
-   option = input('Show Full History (\'h\'), Show History Entry (\'e\') or Make Operation (\'o\')\n').lower()
-   while option != 'h' and option != 'e'and option != 'o':
-      option = input('Show Full History (\'h\'), Show History Entry (\'e\') or Make Operation (\'o\')\n').lower()
-   if option == 'o':
-      operation()
-   elif (option == 'e' or option == 'h') and len(history) == 0:
-      print('No history to show!')
-   elif option == 'e':
-      try:
-         entry = int(input(f'Insert history entry ID from 0 to {len(history) - 1}:'))
-         if entry >= 0 and entry < len(history):
-            print(f'[{entry}]: {history[entry]}')
-         else: 
-            print('Out of range!')
-      except:
-         print('It\'s not a number!')
-   else:
-      for i in range(0, len(history)):
-         print(f'[{i}]: {history[i]}')
-   answer = input('Do you want to go back to menu? (Y/N)\n').lower()
-   while answer != 'y' and answer != 'n':
-      answer = input('Do you want to go back to menu? (Y/N)\n').lower()
-   is_calculator_on = answer == 'y'
+    option = input('Show Full History (\'h\'), Show History Entry (\'e\') or Make Operation (\'o\')\n').lower()
+    while option != 'h' and option != 'e'and option != 'o':
+        option = input('Show Full History (\'h\'), Show History Entry (\'e\') or Make Operation (\'o\')\n').lower()
+    if option == 'o':
+        operation()
+    elif (option == 'e' or option == 'h') and len(history) == 0:
+        print('No history to show!')
+    elif option == 'e':
+        try:
+            entry = int(input(f'Insert history entry ID from 0 to {len(history) - 1}:'))
+            if entry >= 0 and entry < len(history):
+                print(f'[{entry}]: {history[entry]}')
+            else:
+                print('Out of range!')
+        except:
+            print('It\'s not a number!')
+    else:
+        for i in range(0, len(history)):
+            print(f'[{i}]: {history[i]}')
+    answer = input('Do you want to go back to menu? (Y/N)\n').lower()
+    while answer != 'y' and answer != 'n':
+        answer = input('Do you want to go back to menu? (Y/N)\n').lower()
+    is_calculator_on = answer == 'y'
---
+++ ana_borges/calculator_concat_sum.py
@@ -0,0 +1,11 @@
+#Challenge 5 - Ana & Mari
+
+from calculator import Calculator
+
+class CalculatorConcatSum(Calculator):
+    def sum(self):
+        int_n1 = int(self.number1)
+        int_n2 = int(self.number2)
+        concat = float(str(int_n1) + str(int_n2))
+        print(f'The concatenation is: {self.number1} concat {self.number2} = {concat}')
+        return concat
