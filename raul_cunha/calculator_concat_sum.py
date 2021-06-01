83250df8458be9cb7535c08055458ce5444870ca
Raul Cunha <raul.cunha@ab-inbev.com>
2021-06-01 11:26:31 -0300

    [BEESQM-5434] [refactor] and [feat] Create a new Improvement Class Calculator and its methods soma, subtrai, multiplica and divide. Also created a Class CalculatorConcatSum which extends class Calculator in calculator_concat_sum.py, and defining a new method soma, which concatenates two numbers as two strings instead of summing them.

---
+++ raul_cunha/calculator_concat_sum.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python3
+# File: calculator_concat_sum.py (here a new class defined which extends Calculator class and
+# redefines the method "soma" (sum). Instead of summing two numbers, the method will concatenate these numbers as strings
+
+from calculator import *
+
+class CalculatorConcatSum(Calculator):
+    def __init__(self, oper1, oper2):
+        # Just initializing the attributes
+        self.oper1 = oper1
+        self.oper2 = oper2
+    def soma(self):
+        #print('ENTROU NA CALCULATOR CONCAT SUM !!!')
+        resultado = str(self.oper1) + str(self.oper2)
+        #print('EXIBINDO CONCAT: ', str(self.oper1) + str(self.oper2))
+        return (resultado)
