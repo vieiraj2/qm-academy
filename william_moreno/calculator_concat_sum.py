393eec197ad3ee5ca4d0d6658f24dea730f83166
William Ribeiro Moreno <william.ribeiro@ab-inbev.com>
2021-06-01 15:53:53 -0300

    [fix] fix the errors found by inspection

--- william_moreno/calculator_concat_sum.py
+++ william_moreno/calculator_concat_sum.py
@@ -5,5 +5,5 @@ class CalculatorConcatSum(Calculator):
 
     def sum(self):
         print(f"O resultado da soma concatenada é {str(self._a) + str(self._b)}\n")
-        opResult = [self._a, "+", self._b, "=", (str(self._a) + str(self._b))]
-        return opResult
+        op_result = [self._a, "+", self._b, "=", (str(self._a) + str(self._b))]
+        return op_result
