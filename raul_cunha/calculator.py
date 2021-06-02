e4a777c86f9c9e8a5e324d69f720ad5a0c672668
Raul Cunha <raul.cunha@ab-inbev.com>
2021-06-02 11:42:09 -0300

    [BEESQM-5434] [refactor] Solves warning messages displayed on Inspect Code internal tool in Pycharm

--- raul_cunha/calculator.py
+++ raul_cunha/calculator.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
 # New file: calculator.py (instead of calc.py)
-# Program based on first Calculator version which considered integer numbers - Addition, Subtraction, Multiplication and Division
+# Program based on first Calculator version which considered integer numbers - Addition, Subtraction, Multiplication a
+# nd Division
 # IMPROVEMENTS - History of operations
 # This actual version contains a refactoring of code, including classes and methods instead
-# And also is considering the new file "calculator_concat_sum.py" which has a new implementation (Concat) for the method "soma" (sum).
+# And also is considering the new file "calculator_concat_sum.py" which has a new implementation (Concat) for the
+# method "soma" (sum).
 
 from calculator_concat_sum import *
+
 
 class Calculator:
     def __init__(self, oper1, oper2):
         # Just initializing the attributes
         self.oper1 = oper1
         self.oper2 = oper2
+
     def soma(self):
-        return (self.oper1 + self.oper2)
+        return self.oper1 + self.oper2
+
     def subtrai(self):
-        return (self.oper1 - self.oper2)
+        return self.oper1 - self.oper2
+
     def multiplica(self):
-        return (self.oper1 * self.oper2)
+        return self.oper1 * self.oper2
+
     def divide(self):
         try:
             return self.oper1 / self.oper2
@@ -25,14 +32,14 @@ class Calculator:
             print('It does not exist a division by zero!')
             print('\n')
 
+
 def main():
     continua = True     # variavel de controle do loop principal WHILE
-    Historic = []      # Lista p/ armazenamento de informações de operações para Histórico
+    historic = []      # Lista p/ armazenamento de informações de operações para Histórico
     while continua:
-        oper_valids = False
-        oper1 = oper2 = 1
+
         print('*** CALCULATOR FOR INTEGER VALUES ***')
-     #   print('\n')
+#   print('\n')
         print('1 - Addition')
         print('2 - Subtraction')
         print('3 - Multiplication')
@@ -41,10 +48,10 @@ def main():
         print('6 - Quit')
         operacao = input('Type the option of operation, and after press <ENTER>: ')
         print('\n')
-        if  operacao == '6':
+        if operacao == '6':
             continua = False
             continue
-        elif  operacao == '5':
+        elif operacao == '5':
             if len(Historic) == 0:
                 print('Historic of Operations is empty!')
                 print('\n')
@@ -58,19 +65,19 @@ def main():
 
             if option_hist == '1':
                 print('EXIBIR TODO HISTORICO')
-                for j in range(len(Historic)):
-                    #print('Operation ', j+1, ': ', Historic[j])
-                    print(Historic[j])
+                for j in range(len(historic)):
+                    print(historic[j])
                     print('\n')
             elif option_hist == '2':
                 try:
                     option_oper = int(input('Type which operation would you like to search: '))
-                except:
+                except Exception as err:
+                    print(err)
                     print('Invalid input(s)!')
                     print('\n')
                     continue
                 if option_oper != 0:
-                    print(Historic[option_oper - 1])
+                    print(historic[option_oper - 1])
                     print('\n')
                 else:
                     print('Invalid input(s)!')
@@ -78,29 +85,29 @@ def main():
                 print('\n')
                 continue
 
-        elif  operacao == '1':
+        elif operacao == '1':
             print('* Addition *')
             try:
-                oper1 = int(input('Input the first operator, and after press <ENTER>: '))
-                oper2 = int(input('Input the second operator, and after press <ENTER>: '))
-            except:
+                oper1 = float(input('Input the first operator, and after press <ENTER>: '))
+                oper2 = float(input('Input the second operator, and after press <ENTER>: '))
+            except Exception:
                 print('Invalid input(s)!')
                 print('\n')
                 continue
 
             # Constrói uma instância do objeto "Calculator", com os parâmetros inputados pelo usuário
-            ObjCalc = CalculatorConcatSum(oper1, oper2)
+            objcalc = CalculatorConcatSum(oper1, oper2)
 
             # Executa o método "soma", tendo como entrada os valores inputados pelo usuário, e utilizados no
             # instanciamento do objeto
-            resultado = ObjCalc.soma()
+            resultado = objcalc.soma()
 
             # Imprime o resultado da operação
             print('Result of operation: ', resultado)
 
             # GUARDA NO HISTORICO (LISTA)
             content = (oper1, " + ", oper2, ' = ', resultado)
-            Historic.append(content)
+            historic.append(content)
             print('\n')
 
         elif operacao == '2':
@@ -108,24 +115,24 @@ def main():
             try:
                 oper1 = int(input('Input the first operator, and after press <ENTER>: '))
                 oper2 = int(input('Input the second operator, and after press <ENTER>: '))
-            except:
+            except Exception:
                 print('Invalid input(s)!')
                 print('\n')
                 continue
 
             # Constrói uma instância do objeto "Calculator", com os parâmetros inputados pelo usuário
-            ObjCalc = CalculatorConcatSum(oper1, oper2)
+            objcalc = CalculatorConcatSum(oper1, oper2)
 
             # Executa o método "subtrai", tendo como entrada os valores inputados pelo usuário, e utilizados no
             # instanciamento do objeto
-            resultado = ObjCalc.subtrai()
+            resultado = objcalc.subtrai()
 
             # Imprime o resultado da operação
             print('Result of operation: ', resultado)
 
             # GUARDA NO HISTORICO (LISTA)
             content = (oper1, " - ", oper2, ' = ', resultado)
-            Historic.append(content)
+            historic.append(content)
             print('\n')
 
         elif operacao == '3':
@@ -133,24 +140,24 @@ def main():
             try:
                 oper1 = int(input('Input the first operator, and after press <ENTER>: '))
                 oper2 = int(input('Input the second operator, and after press <ENTER>: '))
-            except:
+            except Exception:
                 print('Invalid input(s)!')
                 print('\n')
                 continue
 
             # Constrói uma instância do objeto "Calculator", com os parâmetros inputados pelo usuário
-            ObjCalc = CalculatorConcatSum(oper1, oper2)
+            objcalc = CalculatorConcatSum(oper1, oper2)
 
             # Executa o método "multiplica", tendo como entrada os valores inputados pelo usuário, e utilizados no
             # instanciamento do objeto
-            resultado = ObjCalc.multiplica()
+            resultado = objcalc.multiplica()
 
             # Imprime o resultado da operação
             print('Result of operation: ', resultado)
 
             # GUARDA NO HISTORICO (LISTA)
             content = (oper1, " * ", oper2, ' = ', resultado)
-            Historic.append(content)
+            historic.append(content)
             print('\n')
 
         elif operacao == '4':
@@ -158,24 +165,24 @@ def main():
             try:
                 oper1 = int(input('Input the first operator, and after press <ENTER>: '))
                 oper2 = int(input('Input the second operator, and after press <ENTER>: '))
-            except:
+            except Exception:
                 print('Invalid input(s)!')
                 print('\n')
                 continue
 
             # Constrói uma instância do objeto "Calculator", com os parâmetros inputados pelo usuário
-            ObjCalc = CalculatorConcatSum(oper1, oper2)
+            objcalc = CalculatorConcatSum(oper1, oper2)
 
             # Executa o método "divide", tendo como entrada os valores inputados pelo usuário, e utilizados no
             # instanciamento do objeto
-            resultado = ObjCalc.divide()
+            resultado = objcalc.divide()
 
-            if resultado != None:
+            if resultado is not None:
                 # Imprime o resultado da operação
                 print('Result of operation: ', resultado)
                 # GUARDA NO HISTORICO (LISTA)
                 content = (oper1, " / ", oper2, ' = ', resultado)
-                Historic.append(content)
+                historic.append(content)
                 print('\n')
             else:
                 continue
@@ -188,4 +195,6 @@ def main():
             continua = False
         print('\n')
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    main()
--- raul_cunha/calculator_concat_sum.py
+++ raul_cunha/calculator_concat_sum.py
@@ -1,14 +1,18 @@
 # !/usr/bin/env python3
 # File: calculator_concat_sum.py (here a new class defined which extends Calculator class and
-# redefines the method "soma" (sum). Instead of summing two numbers, the method will concatenate these numbers as strings
+# redefines the method "soma" (sum). Instead of summing two numbers, the method will concatenate these numbers as
+# strings
 
 from calculator import *
+
 
 class CalculatorConcatSum(Calculator):
     def __init__(self, oper1, oper2):
         # Just initializing the attributes
         self.oper1 = oper1
         self.oper2 = oper2
+        super().__init__(oper1, oper2)
+
     def soma(self):
         resultado = str(self.oper1) + str(self.oper2)
-        return (resultado)
+        return resultado
