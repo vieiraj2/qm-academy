c86379d7b763c412a5da632818d6d10b11a4b6ca
DouradoD <39751333+DouradoD@users.noreply.github.com>
2021-06-08 18:33:01 -0300

    [feat] Create a file.py with some methods to test with pytest and pyHamcrest, and include the PyHamcrest lib on requirements.txt (#55)
    Co-authored-by: Dourado <a@s.com>

---
+++ thainara_gomes/exercicios_python/exercicio_2.py
@@ -0,0 +1,6 @@
+#### Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as
+# informações sobre ele.
+
+tipo = input('Digite algo:')
+print('este é o tipo primitivo é', type(tipo))
+print(' este é com espaços em branco', tipo.isspace())
