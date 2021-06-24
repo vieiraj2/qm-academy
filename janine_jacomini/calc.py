7943eb9c5f9abd5b14ce2ad7c752b7dad7831ab7
jljacomini <84154148+jljacomini@users.noreply.github.com>
2021-06-24 15:00:54 -0300

    [feat][refactor][BEESQM-5428]Include a new file for academy - Challen… (#52)
    * [feat][refactor][BEESQM-5428]Include a new file for academy - Challenge 5: Calculator - structured data
    
    * [Feat] [BEESQM-5428] Refact -Challenge 5: Calculator - structured data
    
    Co-authored-by: janine_ <janine.jacomini@ab-inbev.com>

--- janine_jacomini/calc.py
+++ janine_jacomini/calc.py
@@ -1,8 +1,23 @@
-#Challenge 3 e 4 - Calculator - Team 4 - Sergio e Janine
+#Challenge 3, 4 e 5 - Calculator - Team 4 - Sergio e Janine
+#from calculator import Calculator #importa a classe calculator do arquivo calculator
+from calculator_concat_sum import CalculatorConcatSum
 
-print('\n\033[7;35m'+'********* CALCULADORA *********'+'\033[0;0m')
+print('\033[35m'+'\n***** CALCULADORA *****'+'\033[0;0m')
 log_hist = []
 Fim = True
+result = None
+ResultCalc = CalculatorConcatSum(1,2)
+
+def refazerOper(): #função para refazer operação
+    global Fim
+    while True:
+        op2 = input('\nDeseja fazer outra operação? Digite 1 para continuar ou  0 para sair: ')
+        if op2 == '0':
+            print('\n\033[7;34m' + '***** SAINDO DA APLICAÇÃO *****' + '\033[0;0m')
+            Fim = False
+            break
+        elif op2 != '1' or '0':
+            print('\n\033[7;31m' + 'ERRO! Por favor, insira uma opção válida: "1" ou "0"\033[0;0m')
 
 while Fim:
     operacao = input('\nBem-vindo! Você pode fazer 5 tipos de operações: \n\n1-Adição \n2-Subtração \n3-Multiplicação \n4-Divisão  \n5-Histórico \n\nEscolha o tipo de operação: ')
@@ -10,24 +25,20 @@ while Fim:
     try:
         if operacao== '1':
             temp = '+'
-            n1 = float(input('Digite o primeiro número: '))
-            n2 = float(input('Digite o segundo número: '))
-            result = n1 + n2
+            result = ResultCalc.sum()
+
         elif operacao== '2':
             temp='-'
-            n1 = float(input('Digite o primeiro número: '))
-            n2 = float(input('Digite o segundo número: '))
-            result = n1 - n2
+            result = ResultCalc.sub()
+
         elif operacao== '3':
             temp = '*'
-            n1 = float(input('Digite o primeiro número: '))
-            n2 = float(input('Digite o segundo número: '))
-            result = n1 * n2
+            result = ResultCalc.mult()
+
         elif operacao== '4':
             temp = '/'
-            n1 = float(input('Digite o primeiro número: '))
-            n2 = float(input('Digite o segundo número: '))
-            result = n1 / n2
+            result = ResultCalc.div()
+
         elif operacao == '5':
             operacao_hist = input('\n1-Ultimas execuções \n2-Histórico por posição \n\nEscolha o tipo de histórico: ')
             if operacao_hist == '1':
@@ -46,7 +57,7 @@ while Fim:
                     print('\033[7;33m\nHistórico vazio\033[0;0m')
             elif operacao_hist == '2':
                 posicao_hist = int(input('\nQual posição deseja? '))
-                if len(log_hist) >= posicao_hist and posicao_hist > 0:
+                if len(log_hist) >= posicao_hist > 0:
                     posicao_escolhida = posicao_hist
                     posicao_hist-=1 #decrementando pois a lista começa na posição zero
                     print('\n\033[1;96m----------------------------------------------\033[0;0m')
@@ -61,53 +72,15 @@ while Fim:
             elif operacao_hist != '1' or '2':
                 print('\n\033[7;33m'+'Opção inválida!!! Voltando ao menu inicial...'+'\033[0;0m')
                 continue
-            while True:
-              op2 = input('\nDeseja fazer outra operação? Digite 1 para continuar ou  0 para sair: ')
-              if op2 == '1':
-                  run = True
-                  False
-                  break
-              elif op2 == '0':
-                  print('\n\033[7;34m'+'***** SAINDO DA APLICAÇÃO *****'+'\033[0;0m')
-                  Fim = False
-                  False
-                  break
-              elif op2 != '1' or '0':
-                  print('\n\033[7;31m'+'ERRO! Por favor, insira uma opção válida: "1" ou "0"\033[0;0m')
-                  run = True
-        else:
-            print('\n\n\033[7;33m'+'Opção inválida!!!!!!'+'\033[0;0m')
-            continue
+
+            refazerOper()
+
         if operacao != '5':
-            print('\n\033[1;32m'+'Resultado: '+'\033[0;0m' + str(n1) + ' ' + str(temp) + ' ' + str(n2) + ' = ' + str(result))
-            log_hist.append('\033[0;0m' + str(n1) + ' ' + str(temp) + ' ' + str(n2) + ' = ' + str(result))
-            while True:
-              op2 = input('\nDeseja fazer outra operação? Digite 1 para continuar ou  0 para sair: ')
-              if op2 == '1':
-                  run = True
-                  False
-                  break
-              elif op2 == '0':
-                  print('\n\033[7;34m'+'***** SAINDO DA APLICAÇÃO *****'+'\033[0;0m')
-                  Fim = False
-                  False
-                  break
-              elif op2 != '1' or '0':
-                  print('\n\033[7;31m'+'ERRO! Por favor, insira uma opção válida: "1" ou "0"\033[0;0m')
-                  run = True
+            print('\n\033[1;32m'+'Resultado: '+'\033[0;0m' + str(ResultCalc.n1) + ' ' + str(temp) + ' ' + str(ResultCalc.n2) + ' = ' + str(result))
+            log_hist.append('\033[0;0m' + str(ResultCalc.n1) + ' ' + str(temp) + ' ' + str(ResultCalc.n2) + ' = ' + str(result))
+
+            refazerOper()
+
     except:
         print('\n\033[7;31m'+'------ FALHA NA OPERAÇÃO ------\033[0;0m')
-        while True:
-              op2 = input('\nDeseja fazer outra operação? Digite 1 para continuar ou  0 para sair: ')
-              if op2 == '1':
-                  run = True
-                  False
-                  break
-              elif op2 == '0':
-                  print('\n\033[7;34m'+'***** SAINDO DA APLICAÇÃO *****'+'\033[0;0m')
-                  Fim = False
-                  False
-                  break
-              elif op2 != '1' or '0':
-                  print('\n\033[7;31m'+'ERRO! Por favor, insira uma opção válida: "1" ou "0"\033[0;0m')
-                  run = True
+        refazerOper()
