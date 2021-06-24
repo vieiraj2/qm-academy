52b3c03eaca7c3d803493fe6ef5d2931380a1f10
Rayan Alves Ferreira <40619677+RayanFerreira@users.noreply.github.com>
2021-06-24 16:28:12 -0300

    [BEESQM-5420][feat] Create calculator,calculator_concat_sum classes +… (#49)
    * [BEESQM-5246][feat] Create a calculator code + user interface (UI)
    
    * [BEESQM-5246][feat] Change the calcUI.py filename to calc.py
    
    * [BEESQM-5420][feat] Create calculator,calculator_concat_sum classes + exception for division by zero
    
    * [BEESQM-5420][fix] Add operation using negative values + inspection errors
    
    * [BEESQM-5420][feat] Create calculator,calculator_concat_sum classes + exception for division by zero
    
    * [BEESQM-5420][fix] Add operation using negative values + inspection errors
    
    * [BEESQM-5417][feat] Add a history feature for each operations performed
    
    * [BEESQM-5417][fix] Add operation using negative values + inspection errors
    
    * [BEESQM-5246][feat] Create a calculator code + user interface (UI)
    
    * [BEESQM-5246][feat] Change the calcUI.py filename to calc.py
    
    * [BEESQM-5420][feat] Create calculator,calculator_concat_sum classes + exception for division by zero
    
    * [BEESQM-5420][fix] Add operation using negative values + inspection errors
    
    * [BEESQM-5246][feat] Create a calculator code + user interface (UI)
    
    * [BEESQM-5246][feat] Change the calcUI.py filename to calc.py
    
    * [BEESQM-5420][feat] Create calculator,calculator_concat_sum classes + exception for division by zero
    
    * [BEESQM-5420][fix] Add operation using negative values + inspection errors
    
    * [BEESQM-5246][feat] Create a calculator code + user interface (UI)
    
    * [BEESQM-5246][feat] Change the calcUI.py filename to calc.py
    
    * [Refactor]Inspection code fixes
    
    Co-authored-by: William Ribeiro Moreno <william.ribeiro@ab-inbev.com>
    Co-authored-by: Rayan Ferreira <rayan.ferreira@ab-inbev.com>

--- rayan_ferreira/calcBaseCode.py
+++ rayan_ferreira/calcBaseCode.py
@@ -1,59 +1,82 @@
+from calculator_concat_sum import *
 import os
 
+cal_history = []
 option = None
+
 
 def clear():
     os.environ["TERM"] = "linux"
     os.system("clear")
 
 
-def sum_calc(a, b):
-    print(f"O resultado da soma é {a + b}\n")
+def history(op_result):
+    cal_history.append(op_result)
 
 
-def subtraction_calc(a, b):
-    print(f"O resultado da subtração é {a - b}\n")
+def is_float(a):
+    try:
+        float(a)
+        return True
+    except ValueError:
+        return False
 
 
-def multiplication_calc(a, b):
-    print(f"O resultado da multiplicação é {a * b}\n")
+def is_int(a):
+    try:
+        int(a)
+        return True
+    except ValueError:
+        return False
 
 
-def division_calc(a, b):
-    print(f"O resultado da divisão é {a / b}\n")
+def choose_operation(choose_option, a, b):
+    calculator = CalculatorConcatSum(a, b)
+    if choose_option == 'a' or choose_option == 'soma' or choose_option == '1':
+        op_result = calculator.sum()
+        history(op_result)
+    elif choose_option == 'b' or choose_option == 'subtração' or choose_option == '2':
+        op_result = calculator.sub()
+        history(op_result)
+    elif choose_option == 'c' or choose_option == 'multiplicação' or choose_option == '3':
+        op_result = calculator.mult()
+        history(op_result)
+    elif choose_option == 'd' or choose_option == 'divisão' or choose_option == '4':
+        op_result = calculator.div()
+        if op_result:
+            history(op_result)
 
 
-def chooseOperation(chooseOption, a, b):
-    if chooseOption == 'a' or chooseOption == 'soma' or chooseOption == '1':
-        sum_calc(a, b)
-    elif chooseOption == 'b' or chooseOption == 'subtração' or chooseOption == '2':
-        subtraction_calc(a, b)
-    elif chooseOption == 'c' or chooseOption == 'multiplicação' or chooseOption == '3':
-        multiplication_calc(a, b)
-    elif chooseOption == 'd' or chooseOption == 'divisão' or chooseOption == '4':
-        division_calc(a, b)
+def choose_operation_is_valid(choose_option):
+    if choose_option == 'a' or choose_option == 'soma' or choose_option == '1':
+        return True
+    elif choose_option == 'b' or choose_option == 'subtração' or choose_option == '2':
+        return True
+    elif choose_option == 'c' or choose_option == 'multiplicação' or choose_option == '3':
+        return True
+    elif choose_option == 'd' or choose_option == 'divisão' or choose_option == '4':
+        return True
+    elif choose_option == 'e' or choose_option == 'consultar' or choose_option == 'historico' or choose_option == '5':
+        return True
+    else:
+        return False
 
 
-def inputValue(msg,operation, inputNumber):
+def input_value(msg):
     while True:
         value = input(msg)
-        if value.isnumeric():
-            if (operation == 'd' or operation == 'divisão' or operation == '4') and (inputNumber == 2):
-                value = float(value)
-                if value == 0:
-                    print("\033[0;31mNão é possível realizar uma divisão por zero, digite outro valor\033[m")
-                else:
-                    break
-            else:
-                value = float(value)
-                break
+        if is_int(value):
+            value = int(value)
+            break
+        elif is_float(value):
+            value = float(value)
+            break
         else:
             print("\033[0;31mO valor digitado não é um valor válido, digite novamente!\033[m")
     return value
 
 
-def leiaStr (msg):
-
+def leia_str(msg):
     while True:
         value = input(msg)
         if len(value) == 0:
@@ -69,14 +92,61 @@ while option != '2':
     print("b. Subtração")
     print("c. Multiplicação")
     print("d. Divisão")
-    operation = leiaStr("Digite a opção desejada: ")
+    print("e. Consultar o histórico")
+    operation = leia_str("Digite a opção desejada: ")
 
-    value1 = inputValue("Digite o primeiro valor: ", operation, 1)
-    value2 = inputValue("Digite o segundo valor: ", operation, 2)
+    while not choose_operation_is_valid(operation):
+        operation = leia_str("\033[0;31mVocê digitou uma opção inválida. Digite novamente a opção desejada: \033[m")
 
-    chooseOperation(operation, value1, value2)
+    if not (operation == 'e' or operation == 'consultar' or operation == 'historico' or operation == '5'):
+        value1 = input_value("Digite o primeiro valor: ")
+        value2 = input_value("Digite o segundo valor: ")
 
-    print("O que deseja fazer agora?")
+        choose_operation(operation, value1, value2)
+    elif operation == 'e' or operation == 'consultar' or operation == 'historico' or operation == '5':
+        print("\nDeseja verificar o histórico total ou parcial?")
+        print("\n1. Histórico total")
+        print("2. Histórico parcial\n")
+        operation_history = input("Por favor, digite a opção desejada: ")
+
+        if operation_history == '1' or operation_history == 'total':
+            count = 1
+            if len(cal_history) <= 0 or type(cal_history) is None:
+                print("\n\033[0;31mVocê ainda não possui histórico de operações \033[m")
+            else:
+                for i in cal_history:
+                    print(f"Operação #{count}: {i[0]} {i[1]} {i[2]} {i[3]} {i[4]}")
+                    count += 1
+        elif operation_history == '2' or operation_history == 'parcial':
+            count = 1
+            if len(cal_history) <= 0:
+                print("\n\033[0;31mVocê ainda não possui histórico de operações \033[m")
+            else:
+                print("\nVeja abaixo as posições disponíveis no histórico: ")
+                for i in cal_history:
+                    print(f"Operação #{count}")
+                    count += 1
+
+                operation_history_parcial = input("\nPor favor, digite qual posição deseja consultar: ")
+
+                while True:
+                    if int(operation_history_parcial) <= 0 or int(operation_history_parcial) > (count - 1):
+                        print("\n\033[0;31mEssa posição não existe no nosso histórico. "
+                              "Por favor verifique as opções válidas a seguir\033[m")
+                        count2 = 1
+                        for i in cal_history:
+                            print(f"Operação #{count2}")
+                            count2 += 1
+                        operation_history_parcial = input("\nPor favor, digite qual posição deseja consultar: ")
+                    else:
+                        count3 = 1
+                        for i in cal_history:
+                            if count3 == int(operation_history_parcial):
+                                print(f"Operação #{count3}: {i[0]} {i[1]} {i[2]} {i[3]} {i[4]}")
+                            count3 += 1
+                        break
+
+    print("\nO que deseja fazer agora?")
     print("1- Fazer outra operação \n2- Encerrar o programa\n")
     option = input()
     clear()
