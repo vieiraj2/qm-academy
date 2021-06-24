a96b07c41bfbee79416fef2a825fea207d570f9f
Thainara Mota Gomes <80351449+GomesThainara@users.noreply.github.com>
2021-06-24 17:32:38 -0300

    Feature/challenge 5 (#60)
    * [BEESQM-5356][feat]Include historic the operation
    
    * [BEESQM-5216][fix] Include type_value
    
    * [Refector]Refector the code
    
    Co-authored-by: Thainara Gomes <thainara.gomes@ab-inbev.com>

--- thainara_gomes/calc.py
+++ thainara_gomes/calc.py
@@ -1,72 +1,98 @@
-### Introdução - Menu
+from calculator_concat_sum import CalculatorConcatSum
+
+calc = CalculatorConcatSum()
 continua = True
+Historico = []
+
 while continua:
-    print('Olá, Selecione a opção abaixo')
+    print('Calculadora!, Selecione a opção abaixo')
     print(' 1 Soma de dois números (+)'
-      '\n 2 Subtração entre dois números (-)'
-      '\n 3 Multiplicação de dois números (*)'
-      '\n 4 Divisão de dois números  (/)')
+          '\n 2 Subtração entre dois números (-)'
+          '\n 3 Multiplicação de dois números (*)'
+          '\n 4 Divisão de dois números  (/)'
+          '\n 5 Histórico')
 
-##### Segundo passo
+    # Seleção_Histórico!
+
     num = input('Digite uma opção:')
-    if num =='1':
-       print('Opção Soma Selecionada')
-       try:
-         num1 = int(input('Digite o primeiro numero'))
-         num2 = int(input('Digite o segundo numero'))
-         print('Resultado: {} '.format(num1 + num2))
-       except:
-         print ('Digite um numero valido')
+    if num == '1':
+        print('Opção Soma Selecionada')
 
+        num1 = int(input('Digite o primeiro numero'))
+        num2 = int(input('Digite o segundo numero'))
+        print('Resultado: {} '.format(num1 + num2))
+        # Histórico_registro soma
+        resultado = calc.sum(num1, num2)
+        his = (num1, '+', num2, '=', resultado)
+        Historico.append(his)
+    # SelectorSubtract
     elif num == '2':
         print('Opção subtração selecionada')
-        try:
-            num1 = int(input('Digite o primeiro numero'))
-            num2 = int(input('Digite o segundo numero'))
-            print('Resultado: {} '.format(num1 - num2))
-        except:
-            print ('Digite um numero valido')
+        num1 = int(input('Digite o primeiro numero'))
+        num2 = int(input('Digite o segundo numero'))
+        print('Resultado: {} '.format(num1 - num2))
+        # Historic - registry subtract
+        resultado = calc.sub(num1, num2)
+        his = (num1, '-', num2, '=', resultado)
+        Historico.append(his)
 
+    # Selector_Multiplication
     elif num == '3':
         print('Opção Multiplicação selecionada')
-        try:
-            num1 = int(input('Digite o primeiro numero'))
-            num2 = int(input('Digite o segundo numero'))
-            print('Resultado: {} '.format(num1 * num2))
-        except:
-            print ('Digite um numero valido')
+        num1 = int(input('Digite o primeiro numero'))
+        num2 = int(input('Digite o segundo numero'))
+        print('Resultado: {} '.format(num1 * num2))
+        # Histórico_registro_multiplicação
+        resultado = calc.mult(num1, num2)
+        his = (num1, '*', num2, '=', resultado)
+        Historico.append(his)
 
+    # Selector_Divisor
     elif num == '4':
         print('Opção Divisão selecionada')
-        try:
-            num1 = int(input('Digite o primeiro numero'))
-            num2 = int(input('Digite o segundo numero'))
-            if num2 != 0:
-                print('Resultado: {} '.format(num1 / num2))
-            else:
-                print('erro')
-        except:
-            print ('Digite um numero valido')
-
-    elif num =='':
+        num1 = int(input('Digite o primeiro numero'))
+        num2 = int(input('Digite o segundo numero'))
+        if num2 != 0:
+            print('Resultado: {} '.format(num1 / num2))
+        else:
+            print('erro - Não pode ser dividido por zero! ')
+            # Histórico - registro divisão
+            resultado = calc.div(num1, num2)
+            his = (num1, '/', num2, '=', resultado)
+            Historico.append(his)
+    elif num == '':
         print('Selecione uma opção valida')
+        continue
+    elif num == '5':
+        if len(Historico) == 0:
+            print('Histórico de operações está vazio!!')
+            continue
 
+        # Menu de Seleção
+        print('Histórico de Operações')
+        print('1 - Todas as Operações')
+        print('2 - Escolha uma operação específica')
+
+        opcao_hist = input('Selecione uma das opções:')
+        if opcao_hist == '1':
+            print('Exibir todo o histórico')
+            for h in range(len(Historico)):
+                print(Historico[h])
+                print('\n')
+        elif opcao_hist == '2':
+            try:
+                oper = int(input('Digite qual operação você gostaria de pesquisar'))
+            except:
+                print('Opção invalida!')
+                print('\n')
+                continue
+            if oper != 0:
+                print(Historico[oper - 1])
+            else:
+                print('Opção invalida!')
     else:
         print('Digite um numero valido')
 
     final = input('Deseja finalizar? Y/N:')
-    if final =='y' or final =='Y':
+    if final == 'y' or final == 'Y':
         continua = False
-
-
-
-
-
-
-
-
-
-
-
-
-
