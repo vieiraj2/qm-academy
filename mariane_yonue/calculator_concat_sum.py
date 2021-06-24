99100c8cdf4591fff70e1bae8c5b314c5e7b1fe3
Mariane-Yonue <mariane.yonue@gmail.com>
2021-06-24 15:03:13 -0300

    [BEESQM-5400] Fix warnings after pycharm inspect code (#59)
    * [BEESQM-5400] Add classes - Challenge 5
    
    * [BEESQM-5400]Fix warnings after pycharm inspect code
    
    * [BEESQM-5400] Fix warnings after Pycharm inspect code
    
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>

--- mariane_yonue/calculator_concat_sum.py
+++ mariane_yonue/calculator_concat_sum.py
@@ -1,3 +1,5 @@
+# Challenge 5 - Ana & Mari
+
 from calculator import Calculator
 
 
@@ -5,7 +7,6 @@ class CalculatorConcatSum(Calculator):
     def sum(self):
         int_n1 = int(self.number1)
         int_n2 = int(self.number2)
-
         concat = float(str(int_n1) + str(int_n2))
         print(f'The concatenation is: {self.number1} concat {self.number2} = {concat}')
         return concat
