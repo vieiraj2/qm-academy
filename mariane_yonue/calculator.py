fda6855481b4b5e8276f01e4bc9952953ca5f1dc
Ana Cristina <77416348+anacriscborges@users.noreply.github.com>
2021-06-25 11:30:00 -0300

    Feature/rebase (#65)
    * [BEESQM-5098] Include a new file containing a line that should print your name (#10)
    
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    
    * [BEESQM - 5097] Include a new file containing a line that should print your name (#11)
    
    Co-authored-by: Ana Borges <ana.borges@ab-inbev.com>
    
    * [BEESQM-5205]Create method to print types (#23)
    
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    
    * [BEESQM-5204]Create Method to print types (#24)
    
    Co-authored-by: Ana Borges <ana.borges@ab-inbev.com>
    
    * Create calculator (#30)
    
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    
    * Create calculator - Challenge 3 (#29)
    
    Co-authored-by: Ana Borges <ana.borges@ab-inbev.com>
    
    * feature/BEESQM-5255 (#37)
    
    * [BEESQM-5225]Create calculator (#30)
    
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    
    * [BEESQM-5224]Create calculator - Challenge 3 (#29)
    
    Co-authored-by: Ana Borges <ana.borges@ab-inbev.com>
    
    * [BEESQM-5255]Calculator - structured data
    
    * [BEESQM-5255]Calculator - structured data
    
    Co-authored-by: Mariane-Yonue <mariane.yonue@gmail.com>
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    Co-authored-by: Ana Borges <ana.borges@ab-inbev.com>
    
    * [BEESQM-5256] Improves calculator (#38)
    
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    
    * Feature/BEESQM-5399 (#41)
    
    * [BEESQM-5225]Create calculator (#30)
    
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    
    * [BEESQM-5224]Create calculator - Challenge 3 (#29)
    
    Co-authored-by: Ana Borges <ana.borges@ab-inbev.com>
    
    * BEESQM-5399 - Challenge 5
    
    * BEESQM-5399 Challenge 5
    
    * BEESQM-5399
    
    * BEESQM-5399
    
    Co-authored-by: Mariane-Yonue <mariane.yonue@gmail.com>
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    Co-authored-by: Ana Borges <ana.borges@ab-inbev.com>
    
    * [BEESQM-5400] Add classes - Challenge 5 (#42)
    
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    
    * [BEESQM-5400] Fix warnings after pycharm inspect code (#59)
    
    * [BEESQM-5400] Add classes - Challenge 5
    
    * [BEESQM-5400]Fix warnings after pycharm inspect code
    
    * [BEESQM-5400] Fix warnings after Pycharm inspect code
    
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    
    * [BEESQM-5399]Refactor code - Fix warning in Pycharm inspection code (#58)
    
    * [BEESQM-5224]Create calculator - Challenge 3 (#29)
    
    Co-authored-by: Ana Borges <ana.borges@ab-inbev.com>
    
    * BEESQM-5399 - Challenge 5
    
    * BEESQM-5399 Challenge 5
    
    * [BEESQM-5399]Fix warning in Pycharm inspection code
    
    * Merge after rebase
    
    Co-authored-by: Ana Borges <ana.borges@ab-inbev.com>
    
    * [feat] Create a file.py with some methods to test with pytest and pyHamcrest, and include the PyHamcrest lib on requirements.txt (#55)
    
    Co-authored-by: Dourado <a@s.com>
    
    * [feat] setup of pre-commit, pylint and flake8 (#56)
    
    Co-authored-by: Dourado <a@s.com>
    
    * [feat] Create install.sh, contains commands to install the libs and pre-commit (#57)
    
    Co-authored-by: Dourado <a@s.com>
    
    * rename gitignore (#62)
    
    Co-authored-by: Gabriel Goes <gabriel.goes@ab-inbev.com>
    
    Co-authored-by: Mariane-Yonue <mariane.yonue@gmail.com>
    Co-authored-by: Mariane Yonue <mariane-yonue@ab-inbev.com>
    Co-authored-by: Ana Borges <ana.borges@ab-inbev.com>
    Co-authored-by: DouradoD <39751333+DouradoD@users.noreply.github.com>
    Co-authored-by: Dourado <a@s.com>
    Co-authored-by: Gabriel Goes <gabriel.goes@ab-inbev.com>

---
+++ mariane_yonue/calculator.py
@@ -0,0 +1,26 @@
+# Challenge 5 - Ana & Mari
+
+class Calculator:
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
+
+    def sum(self):
+        sum = self.number1 + self.number2
+        print(f'The sum is: {self.number1} + {self.number2} = {sum}')
+        return sum
+        
+    def sub(self):
+        sub = self.number1 - self.number2
+        print(f'The subtraction is: {self.number1} - {self.number2} = {sub}')
+        return sub
+
+    def multi(self):
+        multi = self.number1 * self.number2
+        print(f'The multiplication is: {self.number1} - {self.number2} = {multi}')
+        return multi
+
+    def div(self):
+        div = self.number1 / self.number2
+        print(f'The division is: {self.number1} - {self.number2} = {div}')
+        return div
