63d1748b6b12de161cfb6d35039db6f33e426547
janine_ <janine.jacomini@ab-inbev.com>
2021-07-30 11:37:58 -0300

    [Feat] [BEESQM-5639] Include a new file for academy - Challenge pytest: test_calculator.py

--- janine_jacomini/calculatorMultFunc.py
+++ janine_jacomini/calculatorMultFunc.py
@@ -34,3 +34,23 @@ class calculatorMultFunc(Calculator):
             index = index + 1
             print(oddList)
         return oddList
+
+    # Soma_Calculator
+    def sum(self):
+        soma = self.n1 + self.n2
+        return soma
+
+    # subtracao_Calculator
+    def sub(self):
+        subtracao = self.n1 - self.n2
+        return subtracao
+
+    # Multiplicação_Calculator
+    def mult(self):
+        multiplicacao = self.n1 * self.n2
+        return multiplicacao
+
+    # Divisao_Calculator
+    def div(self):
+        divisao = self.n1 / self.n2
+        return divisao
