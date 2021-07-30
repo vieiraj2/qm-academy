6b5ab39299d75939f86eeebaf05884314e3219c4
Sergio Junior <sergio.junior@ab-inbev.com>
2021-07-30 11:32:24 -0300

    [Feat] BEESQM-5638] Include a new test_calculator for pytest

--- sergio_granzotti/calculatorMultFunc.py
+++ sergio_granzotti/calculatorMultFunc.py
@@ -33,3 +33,23 @@ class calculatorMultFunc(Calculator):
             index = index + 1
             print(lista)
         return lista
+
+    # Soma_Calculator
+    def sum(self):
+        soma = self.n1 + self.n2
+        return soma
+
+    # subtracao_Calculator
+    def sub(self):
+        subtracao = self.n1 - self.n2
+        return subtracao
+
+    # Multiplicação_Calculator
+    def mult(self):
+        multiplicacao = self.n1 * self.n2
+        return multiplicacao
+
+    # Divisao_Calculator
+    def div(self):
+        divisao = self.n1 / self.n2
+        return divisao
