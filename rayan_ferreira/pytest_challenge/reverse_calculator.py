39da9205ca7eb5f0b8708bd3366eab961bc7350f
Rayan Ferreira <rayan.ferreira@ab-inbev.com>
2021-08-11 15:49:26 -0300

    [fix]add inheritance from BaseCalc

--- rayan_ferreira/pytest_challenge/reverse_calculator.py
+++ rayan_ferreira/pytest_challenge/reverse_calculator.py
@@ -3,13 +3,23 @@ from calculator import Calculator as BaseCalc
 
 class ReverseCalc(BaseCalc):
     def soma(self, value1, value2):
-        return value1 - value2
+        return BaseCalc().sub(value1, value2)
 
     def sub(self, value1, value2):
-        return value1 + value2
+        return BaseCalc().soma(value1, value2)
 
     def mult(self, value1, value2):
-        return value1 / value2
+        return BaseCalc().div(value1, value2)
 
     def div(self, value1, value2):
-        return value1 * value2
+        return BaseCalc().mult(value1, value2)
+
+
+# su = ReverseCalc().soma(10, 10)
+# print(su)
+# sub = ReverseCalc().sub(10, 10)
+# print(sub)
+# mult = ReverseCalc().mult(10, 10)
+# print(mult)
+# div = ReverseCalc().div(10, 10)
+# print(div)
