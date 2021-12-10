44c881891add7aa2689f0d356bf9ab1ff8ebdf00
Caio Valente <caio.valente@ab-inbev.com>
2021-12-10 10:15:24 -0300

    [BEESQM-8083][WIP] The application should have an option that allows the user to display all operations executed previously (history of operations) OK The application should have another option that allows the user to verify the operation performed by position WIP

--- caio_valente/calc.py
+++ caio_valente/calc.py
@@ -1,33 +1,64 @@
 def calculator ():
 
-    denominatorIs0 = None
     value1 = None
     value2 = None
     option = None
-    menu = "Choose your option: \n +    -\n *    /\n-"
+    notNumber = False
+
+
+    menu = "Choose your option: \n(+) - Sum\n(-) - Subtraction\n(*) - Multiplication\n(/) - Division\n: "
 
     value1 = float(input("Type the first value: "))
     option = input(menu)
     value2 = float(input("Type the second value: "))
     try:
-        if (option == "+"):
+        if option == "+":
             result = (value1 + value2)
-        elif (option == "-"):
+        elif option == "-":
             result = value1 - value2
-        elif (option == "*"):
+        elif option == "*":
             result = (value1 * value2)
-        elif (option == "/"):
+        elif option == "/":
             result = value1 / value2
 
-        print("Your result is: {}".format(result))
+        print(f"Your result is: {result}")
 
     except:
+        notNumber = True
 
-       if (value2 == 0):
-           print("Can't divide by 0")
+    if value2 == 0:
+        print("Can't divide by 0")
+    elif notNumber:
+        print("That's not a number!")
+    elif result is None:
+        print("Invalid option!")
+    else:
+        calcString = f'{value1} {option} {value2} == {result}'
+        history.append(calcString)
 
-sair = "0"
-while sair == "0":
-    calculator()
-    sair = input("Do you wish to quit? Yes = Any value | No = 0\n")
+history = []
+seeMenu = "y"
+while seeMenu == "y":
+    optionMenu = input("(1) If you wish to see the history of operations\n(2) If you wish to remake a operation\n(3) If you want to make a new operation\n:")
 
+    while optionMenu != "1" and optionMenu != "2" and optionMenu != "3":
+        optionMenu = input("(1) If you wish to see the history of operations\n(2) If you wish to remake an operation\n(3) If you want to make a new operation\n:")
+    if optionMenu == "3":
+        calculator()
+    elif (optionMenu == "1" or optionMenu == "2") and len(history) == 0:
+        print("No history to show!")
+    elif optionMenu == "2":
+        try:
+            entry = int(input(f"Insert an ID from 0 to {len(history) - 1}: "))
+            if 0 <= entry < len(history):
+                print(f"[{entry}]: {history(entry)}")
+            else:
+                print("Out of range!")
+        except:
+            print("That's not a number!")
+    else:
+        for i in range(0, len(history)):
+            print(f"[{i}]: {history[i]}")
+    seeMenu = input("Do you wish to see the menu again? (y|n)\n")
+    if seeMenu != "y" and seeMenu != "n":
+        seeMenu = input("Do you wish to see the menu again? (y|n)\n")
